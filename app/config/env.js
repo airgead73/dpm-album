fee6954e6b23b7051cb35e99c70706c7d86b819b
Dan Moneypenny <catullus1973@gmail.com>
2022-04-02 23:46:23 -0700

    add auth config

--- app/config/env.js
+++ app/config/env.js
@@ -2,10 +2,10 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 /* AUTH */
-// const authSecret = process.env.AUTH_SECRET;
-// const baseUrl = process.env.BASE_URL;
-// const clientId = process.env.CLIENT_ID;
-// const issuerBaseUrl = process.env.ISSUER_BASE_URL;
+const authSecret = process.env.AUTH_SECRET;
+const baseUrl = process.env.BASE_URL;
+const clientId = process.env.CLIENT_ID;
+const issuerBaseUrl = process.env.ISSUER_BASE_URL;
 
 /* DB */
 const mongoUri = process.env.MONGO_URI;
@@ -16,29 +16,29 @@ const mongoUri = process.env.MONGO_URI;
 // const sessionExp = process.env.SESSION_EXP;
 
 /* check variables */
-// if (!authSecret) {
-//   throw new Error(
-//     ".env is missing the definition of an AUTH_SECRET environmental variable",
-//   );
-// }
+if (!authSecret) {
+  throw new Error(
+    ".env is missing the definition of an AUTH_SECRET environmental variable",
+  );
+}
 
-// if (!baseUrl) {
-//   throw new Error(
-//     ".env is missing the definition of a BASE_URL environmental variable",
-//   );
-// }
+if (!baseUrl) {
+  throw new Error(
+    ".env is missing the definition of a BASE_URL environmental variable",
+  );
+}
 
-// if (!clientId) {
-//   throw new Error(
-//     ".env is missing the definition of a CLIENT_ID environmental variable",
-//   );
-// }
+if (!clientId) {
+  throw new Error(
+    ".env is missing the definition of a CLIENT_ID environmental variable",
+  );
+}
 
-// if (!issuerBaseUrl) {
-//   throw new Error(
-//     ".env is missing the definition of an ISSUER_BASE_URL environmental variable",
-//   );
-// }
+if (!issuerBaseUrl) {
+  throw new Error(
+    ".env is missing the definition of an ISSUER_BASE_URL environmental variable",
+  );
+}
 
 if (!mongoUri) {
   throw new Error(
@@ -67,10 +67,10 @@ if (!mongoUri) {
 /* export */
 module.exports = {
   isDev,
-  // authSecret,
-  // baseUrl,
-  // clientId,
-  // issuerBaseUrl,
+  authSecret,
+  baseUrl,
+  clientId,
+  issuerBaseUrl,
   mongoUri,
   // rateLimit,
   // sessionExp,
