f3916a6e87c249b1aed7caa244986f06b95bd661
Dan Moneypenny <catullus1973@gmail.com>
2022-04-04 01:01:40 -0700

    add handlesubmit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DPM Album
 Fullstack application with node, express, and auth0.
 
-### branch 04 ejs
+### branch 05 upload
--- assets/scripts/upload.js
+++ assets/scripts/upload.js
@@ -1,24 +1,67 @@
 const input = document.getElementById('photoInput');
-const display = document.getElementById('photoDisplay');
+const container = document.getElementById('photoDisplay');
 const form = document.getElementById('photoUpload');
-const photo = { src: null, isSelected: false };
 
-const init = ($input, $form) => {
-  console.log('init photo upload')
+function Photo($src) {
+  this.src = $src,
+  this.tag = container,
+  this.display = function() {
+    (this.tag).setAttribute('class', 'show');
+    (this.tag).setAttribute('src', this.src);
+    return;
+  }
+  this.log = function() {
+    console.log("photo:", this.src);
+  }
 }
 
-if(input && form) init(input, form);
+const handleChange = ($target) => {
+  const file = ($target).files[0];
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    const currentPhoto = new Photo(reader.result);
+    currentPhoto.display();
+  }
+}
+
+const postPhoto = ($url) => {
+
+}
+
+const handleSubmit = ($target) => {
+  const inputs = $target.elements;
+  const input = inputs["photoInput"];
+  const file = input.files[0];
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    const currentPhoto = new Photo(reader.result);
+    currentPhoto.log();
+  }
+  
+}
+
+const initInput = ($input) => {
+
+  $input.addEventListener('change', function(event) {
+    console.log('New file has been selected.');
+    handleChange(event.target);
+  });
+
+}
+
+const initForm = ($form) => {
+  $form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    handleSubmit(event.target);
+  });
+}
+
+if(input) initInput(input);
+if(form) initForm(form);
 
 
-// const previewFile = ($file) => {
-//   const reader = new FileReader();
-//   reader.readAsDataURL($file);
-//   reader.onloadend = () => {
-//     const previewSource = reader.result;
-//     console.log("source", previewSource);
-//     displayImg.setAttribute('class', 'show');
-//     displayImg.setAttribute('src', previewSource);
-//   }
 
 // }
 
