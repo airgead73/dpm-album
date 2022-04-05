974b7da4dab1928a9a406b36343236ef9bdc0376
Dan Moneypenny <catullus1973@gmail.com>
2022-04-05 08:03:14 -0700

    add util values

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,10 +1,25 @@
-const camelCase = require('camelcase');
+const d = document;
+const q = {
+  id: function($id) {
+    return d.getElementById($id);
+  },
+  s: function($selector) {
+    return d.querySelector($selector);
+  },
+  all: function($selector) {
+    return d.querySelectorAll($selector);
+  }
+}
 
-const processStr = ($str) => {
-
-  const processedStr = camelCase($str);
-
-  return processedStr;
-
+const a = {
+  set: function($el, $attr, $value) {
+    return $el.setAttribute($attr, $value)
+  },
+  get: function($el, $attr) {
+    return $el.getAttribute($attr);
+  },
+  has: function($el, $attr) {
+    return $el.hasAttribute($attr);
+  }
 }
 
